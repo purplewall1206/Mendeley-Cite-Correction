e9183c670ffd6d6ff59dc456f2ce4bf0ce9368ee
Wang Zi-cheng <wzc@smail.nju.edu.cn>
2021-08-24 21:42:35 +0800

    sync

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Mendeley-Cite-Correction
+
+scholarly api will be blocked by google after a few tries.
+
+selenium also not 
--- bib.py
+++ bib.py
@@ -1,4 +1,4 @@
-from scholarly import scholarly
+import requests
 import os
 
 
@@ -14,23 +14,8 @@ with open('title.txt', 'r') as f:
         x = line[10:i]
         titles.append(x)
 
-print(titles[0])
-search_query = scholarly.search_pubs(titles[0])
-scholarly.pprint(next(search_query))
-# search_query = scholarly.search_pubs('Perception of physical stability and center of mass of 3D objects')
-# scholarly.pprint(next(search_query))
-# paper = scholarly.fill(next(search_query))
-# print(paper)
-# for t in titles:
-#     print(t)
-# search_query = scholarly.search_author('Steven A Cholewiak')
-# author = scholarly.fill(next(search_query))
-# print(author)
-# print([pub['bib']['title'] for pub in author['publications']])
-
-# Take a closer look at the first publication
-# pub = scholarly.fill(author['publications'][0])
-# print(pub)
-
-# Which papers cited that publication?
-# print([citation['bib']['title'] for citation in scholarly.citedby(pub)])
+test = 'eternal memory war'
+print(test)
+payload = {'q' : test, 'format' : 'json', 'h':'1'}
+r = requests.get('https://dblp.org/search/publ/api', params=payload)
+print(r.text)
